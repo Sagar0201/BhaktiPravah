c8723b52e9127df829c6bdf24433273121dec5b6
Sagar0201 <sagarkakade0201@gmail.com>
2025-01-28 00:28:25 +0530

    create model title do some change in infomation model

--- bhakti/admin.py
+++ bhakti/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Category
 from .models import Information
+from .models import Title
 
-admin.site.register(Category)  # Register the Category model to the admin panel
-admin.site.register(Information)  # Register the Information model to the admin panel
+admin.site.register(Category) 
+admin.site.register(Title) 
+admin.site.register(Information)  
+
--- bhakti/models.py
+++ bhakti/models.py
@@ -3,13 +3,28 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=100, unique=True)  # Ensure category names are unique
 
+    class Meta:
+        ordering = ['category_name']  # Automatically sort by category name in ascending order
+
     def __str__(self):
         return self.category_name  # Return the category name as its string representation
 
+    
+class Title(models.Model):
+    name = models.CharField(max_length=255, unique=True)  # Ensure the title name is unique
+
+    class Meta:
+        ordering = ['name']  # Automatically sort by the name field in ascending order
+
+    def __str__(self):
+        return self.name  # String representation of the title
+
 class Information(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='information')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='information',default=None, null=True)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='information',default=None,null=True)  # Connect to Title model
+    heading = models.CharField(max_length=100, default='Bhakti Pravah')
     info = models.TextField()  # Field to store HTML content
     image = models.ImageField(upload_to='information_images/', null=True, blank=True)  # Image field
 
     def __str__(self):
-        return f"Information about {self.category.category_name}"  # String representation with category name
+        return f"Information about {self.title.name} "  # String representation with category name

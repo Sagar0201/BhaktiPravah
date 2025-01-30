e463bec61152fd88c31ddadfd5178cdeb09eb685
Sagar0201 <sagarkakade0201@gmail.com>
2025-01-30 16:34:28 +0530

    single data show ui

--- bhakti/urls.py
+++ bhakti/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from bhakti import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('homepage/', views.homepage, name='homepage'),
+    path('', views.homepage, name='homepage'),
+    path('home/', views.home, name='home'),
+    
+    path('info/<int:info_id>/', views.info, name='info'),
 ]
--- bhakti/views.py
+++ bhakti/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Category
+from django.shortcuts import render, get_object_or_404
+from .models import Information
 
 def home(request):
     categories = Category.objects.all()
@@ -7,4 +9,10 @@ def home(request):
 
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    information_list = Information.objects.only('heading', 'image','id')  
+    return render(request, 'homepage.html',{'information_list': information_list})
+
+
+def info(request,info_id):
+    information = get_object_or_404(Information, id=info_id)
+    return render(request, 'info.html', {'information': information})

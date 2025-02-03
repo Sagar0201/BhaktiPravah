39c44640dd5d9f7db1494dd2cf57f18afe97c2a1
Sagar0201 <sagarkakade0201@gmail.com>
2025-02-04 03:12:39 +0530

    request

--- bhakti/models.py
+++ bhakti/models.py
@@ -38,9 +38,34 @@ class InfoList(models.Model):
     
     # Link info list to multiple Information objects
     information = models.ManyToManyField(Information, related_name='info_lists', blank=True)
+    
 
     class Meta:
         ordering = ['-created_at']  # Newest info lists first
 
     def __str__(self):
         return self.name
+    
+    def add_information(self, information):
+        """
+        Add Information to InfoList and assign it the correct order based on sequence.
+        """
+        # Get the current highest order value in the info list or default to 0
+        current_order = InfoListInformation.objects.filter(info_list=self).count()
+        
+        # Create a new InfoListInformation record with the next order number
+        InfoListInformation.objects.create(info_list=self, information=information, order=current_order + 1)
+    
+    
+    
+class InfoListInformation(models.Model):
+    info_list = models.ForeignKey(InfoList, on_delete=models.CASCADE)
+    information = models.ForeignKey('Information', on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()  # Order of the information in the list
+
+    class Meta:
+        unique_together = ('info_list', 'information')  # Ensure each information appears only once per info list
+        ordering = ['order']  # Ensure that information is ordered by this field
+
+    def __str__(self):
+        return f"{self.info_list.name} - {self.information.heading} (Order: {self.order})"
